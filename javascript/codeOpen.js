4bbbe557920b09350fde88bf9d1d4d649ccd361a
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-04-30 21:45:33 +0900

    <update: javascript 코드 수정>

--- javascript/codeOpen.js
+++ javascript/codeOpen.js
@@ -4,7 +4,7 @@ let isCodeOpen = false; // 코드 박스 오픈 여부
 
 // 버튼을 클릭하여 숨겨져있던 코드 박스 오픈 기능 함수
 function onClickCodeOpenHandler() {
-    if (isCodeOpen) {
+    if (!isCodeOpen) {
         codeBox.style.transform = "translateX(500px)";
     } else {
         codeBox.style.transform = "translateX(0)";
